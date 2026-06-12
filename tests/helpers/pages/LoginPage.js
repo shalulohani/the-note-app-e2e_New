a2aa2333236621ec9411c0464dae9683ebc5b410
Nakshatra Lohani <nakshatralohani@example.com>
2026-06-12 20:49:23 +0530

    Add login method to LoginPage helper

---
+++ tests/helpers/pages/LoginPage.js
@@ -0,0 +1,18 @@
+export class LoginPage {
+  constructor(page) {
+    this.page = page;
+    this.emailInput = page.locator('input[type="email"]');
+    this.passwordInput = page.locator('input[type="password"]');
+    this.loginButton = page.locator('button:has-text("Login")');
+  }
+
+  async gotoLogin() {
+    await this.page.goto('https://pass-the-note-app.vercel.app/login');
+  }
+
+  async login(email, password) {
+    await this.emailInput.fill(email);
+    await this.passwordInput.fill(password);
+    await this.loginButton.click();
+  }
+}
